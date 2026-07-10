09af09a1d9bf0248c4777b1ab1f8facdd5f2cd2d
Karthikeya12724 <karthik66372@gmail.com>
2026-07-10 14:22:54 +0530

    Initial commit

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from "react";
+import Navbar from "../components/Navbar";
+import Header from "../components/Header";
+import About from "../components/About";
+import Event from "./event";
+import EventList from "./EventList";
+import MoreEvent from "./MoreEvent";
+
+
+const Home = () => {
+  const [events, setEvents] = useState([]);
+
+  
+
+  useEffect(() => {
+    const savedEvents = localStorage.getItem("events");
+    if (savedEvents) {
+      setEvents(JSON.parse(savedEvents));
+    }
+  }, []);
+
+  return (
+    <div>
+      <Navbar />
+      <Header />
+      <About />
+      <Event />
+      <MoreEvent events={events} />
+      
+
+      
+      
+    </div>
+  );
+};
+
+export default Home;
